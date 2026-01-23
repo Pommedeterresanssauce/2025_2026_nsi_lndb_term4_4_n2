4ec98dfdb6d1640db798cc1e83c79e4ff495a24a
skibidi Greg <grgr.gllrn@gmail.com>
2026-01-23 15:20:09 +0100

    animations

--- script/main.py
+++ script/main.py
@@ -6,7 +6,7 @@ from settings import *
 class Simulation :
     def __init__(self) :
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.DOUBLEBUF, vsync=1)
         pygame.display.set_caption("Poker 2")
         self.clock = pygame.time.Clock()
         
@@ -14,14 +14,14 @@ class Simulation :
 
     def run(self) :
         while True :
-            self.clock.tick(60)
+            dt = self.clock.tick(0) / 1000.0  # secondes
             events = pygame.event.get()
             for event in events :
                 if event.type == pygame.QUIT :
                     pygame.quit()
                     sys.exit()
 
-            self.table.update()
+            self.table.update(dt)
             pygame.display.update()
 
 if __name__ == "__main__" :
--- script/table.py
+++ script/table.py
@@ -2,59 +2,102 @@ import pygame
 from player import *
 from support import import_folder
 
-class Table :
-    def __init__(self) :
+
+class Table:
+    def __init__(self):
         self.player = Player()
         self.screen = pygame.display.get_surface()
         self.screen_width, self.screen_height = self.screen.get_size()
-        
-        # action in progress
+
+        # phase
         self.active_turn = 'shuffle'
-        
+        self.shuffle_done = False  # <<< LE TRUC IMPORTANT
+
         # images
-        self.table_image = pygame.image.load('graphics/table_de_jeu/table_verte.png')
-        self.table_image = pygame.transform.scale(self.table_image, (self.screen_width, self.screen_height))
-        self.white_rectangle_image = pygame.image.load('graphics/table_de_jeu/rectangle_blanc.png')
-        self.white_rectangle_image = pygame.transform.scale(self.white_rectangle_image, (140, 192))
-        self.deck_image = pygame.image.load('graphics/table_de_jeu/deck.png')
-        self.deck_image = pygame.transform.scale(self.deck_image, (140, 252))
-        
+        self.table_image = pygame.image.load(
+            'graphics/table_de_jeu/table_verte.png'
+        ).convert()
+        self.table_image = pygame.transform.scale(
+            self.table_image, (self.screen_width, self.screen_height)
+        )
+
+        self.white_rectangle_image = pygame.image.load(
+            'graphics/table_de_jeu/rectangle_blanc.png'
+        ).convert_alpha()
+        self.white_rectangle_image = pygame.transform.scale(
+            self.white_rectangle_image, (140, 192)
+        )
+
+        self.deck_image = pygame.image.load(
+            'graphics/table_de_jeu/deck.png'
+        ).convert_alpha()
+        self.deck_image = pygame.transform.scale(
+            self.deck_image, (140, 252)
+        )
+
         # animations
         self.animations = {
-            'shuffle' : [],
+            'shuffle': import_folder('graphics/animations/shuffle')
         }
-        for animation in self.animations.keys() :
-            full_path = 'graphics/animations/' + animation
-            self.animations[animation] = import_folder(full_path)
-        self.animation_index = 0
-        
-        self.actual_animation = 'none'
-        
-        self.animations_pos = {
-            'shuffle' : (800, 150)
+
+        self.actual_animations = []
+
+        self.animations_infos = {
+            'shuffle': {
+                'pos': (625, 102),
+                'index': 0
+            }
         }
-    
-    
-    def update_turn_phase(self) :
-        pass
-    
-    
-    def draw(self) :
-        # affchage de la table
+
+
+    def update_turn_phase(self):
+        # lancer l'animation UNE SEULE FOIS
+        if self.active_turn == 'shuffle' and not self.shuffle_done:
+            if 'shuffle' not in self.actual_animations:
+                self.animations_infos['shuffle']['index'] = 0
+                self.actual_animations.append('shuffle')
+
+        # quand l'animation est terminée
+        if self.active_turn == 'shuffle' and self.shuffle_done:
+            self.active_turn = '1'
+
+
+    def update_and_draw_animations(self, dt):
+        for animation in self.actual_animations.copy():
+            self.animations_infos[animation]['index'] += dt * 10
+            index = int(self.animations_infos[animation]['index'])
+
+            if index >= len(self.animations[animation]):
+                self.actual_animations.remove(animation)
+                if animation == 'shuffle':
+                    self.shuffle_done = True
+            else:
+                pos = self.animations_infos[animation]['pos']
+                self.screen.blit(self.animations[animation][index], pos)
+
+
+    def draw(self):
+        # table
         self.screen.blit(self.table_image, (0, 0))
-        # affichage des rectangle blanc
+
+        # rectangles blancs
         self.screen.blit(self.white_rectangle_image, (700, 420))
         self.screen.blit(self.white_rectangle_image, (1070, 420))
         self.screen.blit(self.white_rectangle_image, (885, 420))
         self.screen.blit(self.white_rectangle_image, (792.5, 657))
         self.screen.blit(self.white_rectangle_image, (977.5, 657))
-        # affichage du deck
-        self.screen.blit(self.deck_image, (885, 150))
-    
-    
-    def update(self) :
+
+        # deck visible seulement après le mélange
+        if self.shuffle_done:
+            self.screen.blit(self.deck_image, (885, 150))
+
+
+    def update(self, dt):
         self.update_turn_phase()
         self.draw()
+        self.update_and_draw_animations(dt)
         self.player.draw()
+
+
     
     
