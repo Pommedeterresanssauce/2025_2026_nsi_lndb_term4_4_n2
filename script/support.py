acb7b3f479d9cf156befca6788275d58ca8e8f0c
skibidi Greg <grgr.gllrn@gmail.com>
2026-01-28 20:02:47 +0100

    on avance sur le systeme de tour de jeu et des gestions de ses phase

--- script/support.py
+++ script/support.py
@@ -5,6 +5,9 @@ def import_folder(path):
     surface_list = []
 
     for _, __, img_files in walk(path):
+        # --- ETAPE CRUCIALE : Trier la liste des noms de fichiers ---
+        img_files.sort() 
+        
         for image in img_files:
             if image.endswith(('.png', '.jpg', '.jpeg')):
                 full_path = path + '/' + image
--- script/table.py
+++ script/table.py
@@ -1,6 +1,7 @@
 import pygame
 from player import *
 from support import import_folder
+import random
 
 
 class Table:
@@ -9,89 +10,130 @@ class Table:
         self.screen = pygame.display.get_surface()
         self.screen_width, self.screen_height = self.screen.get_size()
 
+        # --- PLAYERS --- 
+        self.player1 = Player()
+        self.player2 = Player()
+        self.players = [self.player1, self.player2]
+
         # --- PHASE ---
         self.active_turn = 'shuffle'
         self.shuffle_done = False 
+        self.shuffle_animation_done = False
+        self.time_since_end_shuffle_anim = 0
         self.distribution_done = False
+        self.distribution_animation_done = False
         
         # --- DECK COMPOSITION ---
-        self.deck_cards = [
-            
-        ]
+        self.deck_cards = []
+        card_representation = {"p" : ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'],
+                               "c" : ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'],
+                               "t" : ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'],
+                               "k" : ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13']}
+        for key, values in card_representation.items() :
+            for value in values :
+                self.deck_cards.append(str(key + value))
 
         # --- IMAGES ---
-        self.table_image = pygame.image.load(
-            'graphics/table_de_jeu/table_verte.png'
-        ).convert()
-        self.table_image = pygame.transform.scale(
-            self.table_image, (self.screen_width, self.screen_height)
-        )
+        self.table_image = pygame.image.load('graphics/table_de_jeu/table_verte.png').convert()
+        self.table_image = pygame.transform.scale(self.table_image, (self.screen_width, self.screen_height))
 
-        self.white_rectangle_image = pygame.image.load(
-            'graphics/table_de_jeu/rectangle_blanc.png'
-        ).convert_alpha()
-        self.white_rectangle_image = pygame.transform.scale(
-            self.white_rectangle_image, (140, 192)
-        )
+        self.white_rectangle_image = pygame.image.load('graphics/table_de_jeu/rectangle_blanc.png').convert_alpha()
+        self.white_rectangle_image = pygame.transform.scale(self.white_rectangle_image, (140, 192))
 
-        self.deck_image = pygame.image.load(
-            'graphics/table_de_jeu/deck.png'
-        ).convert_alpha()
-        self.deck_image = pygame.transform.scale(
-            self.deck_image, (140, 252)
-        )
+        self.deck_image = pygame.image.load('graphics/table_de_jeu/deck.png').convert_alpha()
+        self.deck_image = pygame.transform.scale(self.deck_image, (140, 252))
 
         # --- ANIMATIONS ---
         self.animations = {
-            'shuffle': import_folder('graphics/animations/shuffle')
+            'shuffle' : import_folder('graphics/animations/shuffle')
         }
 
         self.actual_animations = []
 
         self.animations_infos = {
-            'shuffle': {
-                'pos': (625, 102),
-                'index': 0
+            # shuffle anim infos
+            'shuffle' : {
+                'pos' : (625, 102),
+                'index' : 0
+            },
+            # distribution anim infos
+            'distribution' : {
+                'card1_pos' : (0, 0),
+                'card2_pos' : (100, 100),
             }
         }
 
 
+    def shuffle_deck(self) :
+        random.shuffle(self.deck_cards)
+        self.shuffle_done = True
+        # print(self.deck_cards)
+
+
+    def distribute_cards(self) :
+        i = 0
+        for player in self.players :
+            player.hand = [self.deck_cards[i], self.deck_cards[i + 1]]
+            i += 2
+            print(player.hand)
+        self.distribution_done = True
+
+
     def turn_action(self) :
-        if self.active_turn == 'distribution' : 
-            pass
-        
-
-
-    def update_turn_phase(self):
-        # lancer l'animation UNE SEULE FOIS
-        if self.active_turn == 'shuffle' and not self.shuffle_done:
-            if 'shuffle' not in self.actual_animations:
+        if self.active_turn == 'shuffle' and not self.shuffle_done :
+            self.shuffle_deck()
+            
+        if self.active_turn == 'distribution' and not self.distribution_done : 
+            self.distribute_cards()
+            
+            
+    def update_turn_phase(self, dt) :
+        # la premiere phase du tout est le melange
+        if self.active_turn == 'shuffle' and not self.shuffle_animation_done :
+            if 'shuffle' not in self.actual_animations :
                 self.animations_infos['shuffle']['index'] = 0
                 self.actual_animations.append('shuffle')
 
-        # quand le mélange est terminé
-        if self.active_turn == 'shuffle' and self.shuffle_done :
-            self.active_turn = 'distribution'
+        # quand le mélange est terminé depuis au moins 0.3 seconde
+        if self.active_turn == 'shuffle' and self.shuffle_done and self.shuffle_animation_done :
+            if self.time_since_end_shuffle_anim > 0.3 :
+                self.active_turn = 'distribution'
+                if 'distribution' not in self.actual_animations :
+                    self.animations_infos['distribution']['index'] = 0
+                    self.actual_animations.append('distribution')
+            else :
+                self.time_since_end_shuffle_anim += dt
             
         # quand la distribution est terminée
-        if self.active_turn == 'distribution' and self.distribution_done :
+        if self.active_turn == 'distribution' and self.distribution_done and self.distribution_animation_done:
             self.active_turn = 'player1'
+            print('okk')
             
-            
+    
+    def update_and_draw_distribution_animation(self, dt) :
+        self.distribution_animation_done = True
+    
+    
+    def update_and_draw_frame_animation(self, animation, dt) :
+        self.animations_infos[animation]['index'] += dt * 12
+        index = int(self.animations_infos[animation]['index'])
+
+        if index >= len(self.animations[animation]):
+            self.actual_animations.remove(animation)
+            if animation == 'shuffle' :
+                self.shuffle_animation_done = True
+        else:
+            pos = self.animations_infos[animation]['pos']
+            self.screen.blit(self.animations[animation][index], pos)
+    
         
-    def update_and_draw_animations(self, dt):
-        for animation in self.actual_animations.copy():
-            self.animations_infos[animation]['index'] += dt * 11
-            index = int(self.animations_infos[animation]['index'])
-
-            if index >= len(self.animations[animation]):
-                self.actual_animations.remove(animation)
-                if animation == 'shuffle':
-                    self.shuffle_done = True
-            else:
-                pos = self.animations_infos[animation]['pos']
-                self.screen.blit(self.animations[animation][index], pos)
-
+    def update_and_draw_animations(self, dt) :
+        for animation in self.actual_animations.copy() :
+            if animation in ['shuffle'] :
+                self.update_and_draw_frame_animation(animation, dt)
+            elif animation == 'distribution' :
+                self.update_and_draw_distribution_animation(dt)
+                
 
     def draw(self):
         # table
@@ -105,15 +147,14 @@ class Table:
         self.screen.blit(self.white_rectangle_image, (977.5, 657))
 
 
-            
     def draw_cards(self) :
         # deck visible seulement après le mélange
-        if self.shuffle_done:
+        if self.shuffle_animation_done :
             self.screen.blit(self.deck_image, (885, 150))
 
 
     def update(self, dt):
-        self.update_turn_phase()
+        self.update_turn_phase(dt)
         self.turn_action()
         self.draw()
         self.update_and_draw_animations(dt)
